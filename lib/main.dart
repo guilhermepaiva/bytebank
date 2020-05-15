e88eea707665403bb83a4f487cc59db2896a3649
Guilherme Paiva <guilhermepaiva@Guilhermes-MBP.home>
2020-05-15 16:27:28 -0300

    added a card to show some informations

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.00'),
+                subtitle: Text('CC: 0007-8'),
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('300.00'),
+                subtitle: Text('CC: 1245-9'),
+              )
+            ),
+          ],
+        ),
         appBar: AppBar(title: Text('Transferências'),),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
