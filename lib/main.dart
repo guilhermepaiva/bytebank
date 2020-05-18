dc68f2ce09c7a67b4cf18a1d7428fa9f98fd4501
Guilherme Paiva <guilhermepaiva@Guilhermes-MBP.home>
2020-05-18 12:00:27 -0300

    some refactor

--- lib/main.dart
+++ lib/main.dart
@@ -26,56 +26,68 @@ class FormularioTransferencia extends StatelessWidget {
       appBar: AppBar(title: Text('Criando transferencia'),),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: TextStyle(
-                fontSize: 24.0
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da conta',
-                hintText: '0000'
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              style: TextStyle(
-                fontSize: 24.0
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0.00'
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: 'Numero da conta',
+              dica: '0000'),
+          Editor(
+              controlador: _controladorCampoValor,
+              rotulo: 'Valor',
+              dica: '0.00',
+              icone: Icons.monetization_on),
           RaisedButton(
-            onPressed: () {
-              final int numeroConta = int.tryParse(_controladorCampoValor.text);
-              final double valor = double.tryParse(_controladorCampoNumeroConta.text);
-
-              if (numeroConta != null && valor != null){
-                final transferenciaCriada = Transferencia(valor, numeroConta);
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('$transferenciaCriada'),
-                  ),
-                );
-              }
-
-            },
+            onPressed: () => _criaTransferencia(context),
             child: Text('Confirmar'),
           )
         ],
       ),
     );
   }
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null){
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('$transferenciaCriada'),
+        ),
+      );
+    }
+  }
 }
+
+class Editor extends StatelessWidget {
+
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: controlador,
+          style: TextStyle(
+              fontSize: 24.0
+          ),
+          decoration: InputDecoration(
+              icon: icone != null ? Icon(icone) : null,
+              labelText: rotulo,
+              hintText: dica,
+          ),
+          keyboardType: TextInputType.number,
+        ),
+    );
+  }
+}
+
 
 
 class ListaTransferencias extends StatelessWidget {
